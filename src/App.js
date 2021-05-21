52b4020e99ace704d88eeb3b203ba4b9028b31eb
aristov <vv.aristov@gmail.com>
2021-05-21 16:19:01 +0300

    Implementing the UI (WIP)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import React from 'react'
 import './App.css'
+
+const items = [
+  '00-01.jpg',
+  '01-0_Квартира_для_родителей.jpg',
+  '01-1_Квартира_для_родителей.jpg',
+]
 
 function App() {
   return (
@@ -8,13 +15,36 @@ function App() {
   )
 }
 
-function SlideShow() {
+class SlideShow extends React.Component
+{
+  constructor(props) {
+    super(props)
+    this.state = {
+      current : 0,
+    }
+  }
+
+  render() {
+    return (
+      <div className="SlideShow">
+        <div className="SlideList"
+             onClick={ () => this.onClick() }
+             style={ { left : -this.state.current * 100 + '%' } }>
+          { items.map((item, i) => <SlideItem key={ i } url={ item }/>) }
+        </div>
+      </div>
+    )
+  }
+
+  onClick() {
+    const current = this.state.current + 1
+    this.setState({ current : current < items.length? current : 0 })
+  }
+}
+
+function SlideItem(props) {
   return (
-    <div className="SlideShow">
-      <div className="Slide" style={ { backgroundImage : 'url(media/00-01.jpg)' } }/>
-      <div className="Slide" style={ { backgroundImage : 'url(media/01-0_Квартира_для_родителей.jpg)' } }/>
-      <div className="Slide" style={ { backgroundImage : 'url(media/01-1_Квартира_для_родителей.jpg)' } }/>
-    </div>
+    <div className="SlideItem" style={ { backgroundImage : `url(media/${ props.url })` } }/>
   )
 }
 

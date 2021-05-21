da89bf22734943be326f2308265e2cab5037b21c
aristov <vv.aristov@gmail.com>
2021-05-21 13:51:19 +0300

    Added the replace-spaces util

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      <SlideShow/>
+    </div>
+  )
+}
+
+function SlideShow() {
+  return (
+    <div className="SlideShow">
       <div className="Slide" style={ { backgroundImage : 'url(media/00-01.jpg)' } }/>
+      <div className="Slide" style={ { backgroundImage : 'url(media/01-0_Квартира_для_родителей.jpg)' } }/>
+      <div className="Slide" style={ { backgroundImage : 'url(media/01-1_Квартира_для_родителей.jpg)' } }/>
     </div>
   )
 }

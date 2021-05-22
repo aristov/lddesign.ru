fb57d9fae7a8a2c50d1522b950a6ced34482b6ae
aristov <vv.aristov@gmail.com>
2021-05-22 18:08:36 +0300

    Implementing the mobile navigation (WIP)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import { SlideShow } from './SlideShow'
 import { Header } from './Header'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <SlideShow/>
-    </div>
-  )
+class App extends React.Component
+{
+  constructor(props) {
+    super(props)
+    this.state = { open : false }
+  }
+
+  onClick = () => {
+    this.setState(state => ({ open : !state.open }))
+  }
+
+  render() {
+    return (
+      <div className={ this.state.open? 'App open' : 'App' }>
+        <div className="Inner">
+          <Header open={ this.state.open } onClick={ this.onClick }/>
+          <SlideShow/>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import './Header.css'
 
-export function Header() {
-  return (
-    <header className="Header">
-      <h1><a href="/">Лариса Дедловская</a></h1>
-      <nav>
-        <ul>
-          <li><a href="/classic">Классика</a></li>
-          <li><a href="/loft">Лофты</a></li>
-          <li><a href="/exterior">Экстерьер</a></li>
-          <li><a href="/design">Проектирование</a></li>
-          <li><a href="/blog">Блог</a></li>
-          <li><a href="/contact">Контакты</a></li>
-        </ul>
-      </nav>
-    </header>
-  )
+export class Header extends React.Component
+{
+  /*constructor(props) {
+    super(props)
+    this.state = { open : false }
+  }
+
+  onClick = () => {
+    this.setState(state => ({ open : !state.open }))
+  }*/
+
+  render() {
+    return (
+      <header className="Header">
+        <div className="Inner">
+          <h1><a href="/">Лариса Дедловская</a></h1>
+          <button className="MenuButton" onClick={ this.props.onClick }>
+            <span className={ this.props.open? 'icon icon-cancel' : 'icon icon-menu' }/>
+          </button>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="/classic">Классика</a></li>
+            <li><a href="/loft">Лофты</a></li>
+            <li><a href="/exterior">Экстерьер</a></li>
+            <li><a href="/design">Проектирование</a></li>
+            <li><a href="/blog">Блог</a></li>
+            <li><a href="/contact">Контакты</a></li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
 }

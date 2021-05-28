341835032481776c35c9b4ab82c0acc2593b1d03
aristov <vv.aristov@gmail.com>
2021-05-28 17:03:49 +0300

    Styling and refactoring

--- src/Contacts.js
+++ src/Contacts.js
@@ -6,39 +6,39 @@ export function Contacts() {
     <div className="Contacts">
       <h2>Контакты</h2>
       <hr/>
-      <a className="SocialLink"
+      <a className="ContactsLink"
          href="mailto:lddesign@mail.ru">
         <span className="icon icon-mail"/>
         lddesign@mail.ru
       </a>
-      <a className="SocialLink"
+      <a className="ContactsLink"
          href="tel:+79257711473">
         <span className="icon icon-phone"/>
         +7 925 771 1473
       </a>
       <hr/>
-      <a className="SocialLink"
+      <a className="ContactsLink"
          href="https://instagram.com/design.ld"
          target="_blank"
          rel="noreferrer">
         <span className="icon icon-instagram"/>
         instagram.com/design.ld
       </a>
-      <a className="SocialLink"
+      <a className="ContactsLink"
          href="https://facebook.com/larisa.dedlovskaya"
          target="_blank"
          rel="noreferrer">
         <span className="icon icon-facebook"/>
         facebook.com/larisa.dedlovskaya
       </a>
-      <a className="SocialLink"
+      <a className="ContactsLink"
          href="https://vk.com/larisadedlovskaya"
          target="_blank"
          rel="noreferrer">
         <span className="icon icon-vkontakte"/>
         vk.com/larisadedlovskaya
       </a>
-      <a className="SocialLink"
+      <a className="ContactsLink"
          href="https://mona.livejournal.com"
          target="_blank"
          rel="noreferrer">
